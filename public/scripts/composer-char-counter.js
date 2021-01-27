f14ae305bdfdf657de2c89bc3ac94701c2bb9976
vagrant <mveri.bruno@gmail.com>
2021-01-27 20:33:28 +0000

    Added logic to counter the number os characters left to input in textarea

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+  let count = 140;
+  let counter = $('.counter');
+  
+  $("#tweet-text").on('input', function() {
+    const char = $(this).val().length;
+    counter.html(count - char);
+    
+    count < char ? counter.css('color', 'red') : counter.css('color', '#545149')
+
+    
+  });
+ 
+
+});
+
